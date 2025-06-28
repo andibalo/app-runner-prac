7d5f7b31fba41f8fa724951101a7b288af0268a1
andibalo <andibalo213@gmail.com>
2025-06-28 19:32:44 +0700

    test codebuild webhook and auto deploy part 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node from Docker. Automatic deployment from github' });
+  res.render('index', { title: 'Node from Docker. Automatic deployment from github part 2' });
 });
 
 module.exports = router;
